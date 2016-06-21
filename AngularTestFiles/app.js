fafdb420a6a19525d7087617f0037ccedce4a0a4
gabriel <ggalarza@v2.com>
2016-06-20 21:12:02 -0400

    directory

--- AngularTestFiles/app.js
+++ AngularTestFiles/app.js
@@ -1,23 +1,28 @@
-var app = angular.module("myApp", []);
+// var app = angular.module("yotpoApp", []);
 
-app.controller("yotpoCtrl", function($scope, $http) {
-	//loadData();
+// app.controller("yotpoCtrl", function($scope, $http) {
+// 	loadData();
 
-	$scope.SendReview = function () {
-		$http.post('http://api.yotpo.com/oauth/token',  {"client_id": "8Gu0wvo2873r7uP7A8hFjhGEhWzXBds8Rl3VEJgu",
- "client_secret": "e6lhE3J89U5kecx8G5jf52QDCzF0iHtzu8zzao4p",
- "grant_type": ""})
-		.success(function (response) {
-			console.log("It works!");		
-		});
+// 	// $scope.SendReview = function () {
+// 	// 	$http.post('http://api.yotpo.com/oauth/token',  {"client_id": "8Gu0wvo2873r7uP7A8hFjhGEhWzXBds8Rl3VEJgu",
+//  // "client_secret": "e6lhE3J89U5kecx8G5jf52QDCzF0iHtzu8zzao4p",
+//  // "grant_type": ""})
+// 	// 	.success(function (response) {
+// 	// 		console.log("It works!");		
+// 	// 	});
 		
 		
-	};
+// 	// };
 
-	//function loadData() {
-	//	$http.get("test.html").success(function (data) {
-	//		$scope.tasks = data;
-	//	});
-	//};
+// 	function loadData() {
+// 		$http.get("widget.html")
+// 		.success(function (response) {
+// 			$scope.widget = response.data;
 
-});
+// 			$('.review').append('widget.html');
+
+// 			console.log('It works!');
+// 		});
+// 	};
+
+// });
